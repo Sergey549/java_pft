60de9318637dadaaa033d144d660997d6ff69d81
Sergey549 <55460819+Sergey549@users.noreply.github.com>
2020-03-21 21:03:13 +0300

    Коммит

--- src/test/java/ru/stqa/pft/sandbox2/SquareTest.java
+++ src/test/java/ru/stqa/pft/sandbox2/SquareTest.java
@@ -1,16 +1,17 @@
-package ru.stqa.pft.sandbox;
+package ru.stqa.pft.sandbox2;
 
 import org.junit.Assert;
 import org.junit.Test;
 import ru.stqa.pft.sandbox.Square;
 
-class SquareTests {
+public class SquareTest {
 
     @Test
-    public void testArea () {
+    public void testArea() {
 
-        Square s = new Square (5);
+        Square s = new Square(5);
         assert s.area() == 25;
 
     }
+
 }
