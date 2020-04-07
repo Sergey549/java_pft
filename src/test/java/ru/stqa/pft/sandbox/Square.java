c1bcdf88f00979ab58b135aa918d9d64a9775a7b
Sergey549 <55460819+Sergey549@users.noreply.github.com>
2020-04-07 13:33:09 +0300

    New

--- src/test/java/ru/stqa/pft/sandbox/Square.java
+++ src/test/java/ru/stqa/pft/sandbox/Square.java
@@ -10,6 +10,7 @@ public class Square {
     }
 
     public double area() {
+
         return this.l * this.l;
     }
 }
---
+++ src/test/java/ru/stqa/pft/sandbox3/p1.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox3;
+
+class Point1 {
+
+    public double x1;
+    public double y1;
+
+    public Point1 (double x1, double y1){
+
+        this.x1 = x1;
+        this.y1 = y1;
+
+    }
+
+
+}
