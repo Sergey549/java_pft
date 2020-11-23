760400898bff83688a0f2ab31d10f279aac5a5c2
User <serega417@gmail.com>
2020-11-23 19:11:08 +0300

    Реализовал сравнение списков в тестах для создания, удаления и модификации товаров. Добавил анонимную функцию (лямбда выражение), заменил на нее цикл для поиска максимального элемента в списке при создании товара (поиск макс. идентификатора необходим, чтобы узнать какой новый идентификатор присвоен новой добавленной группе, в свою очередь это необходимо для сравнения 2-х списков: до и после создания товара. Таким образом новый идентификатор созданного товара в новом списке будет максимальным среди всех имеющихся)

--- litecart-web-tests/src/test/java/litecart/tests/Product/ProductCreationTest.java
+++ litecart-web-tests/src/test/java/litecart/tests/Product/ProductCreationTest.java
@@ -5,6 +5,7 @@ import litecart.tests.TestBase;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -16,18 +17,24 @@ public class ProductCreationTest extends TestBase {
         app.getNavigationHelper().goToCatalog();
         List<ProductData> before = app.getProductHelper().getProductList();
         ProductData group = new ProductData
-                ("Duck1", null, null, null);
+                ("Duck2", null, null, null);
         app.getProductHelper().createProduct(group);
         List<ProductData> after = app.getProductHelper().getProductList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max =0;
-        for (ProductData g: after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        group.setId(max);
+//        int max =0;
+//        for (ProductData g: after) {
+//            if (g.getId() > max) {
+//                max = g.getId();
+//            }
+//        }
+//        group.setId(max);
+
+        // Преобразовал цикл, вводим анонимную функцию (лямбда выражение) - >
+        // Новый список превращаем в поток.
+        // У потока есть метод max, в который позволяет вычислять макс. элемент.
+        // Передаем компаратор (сравниватель) в качестве параметра. Получаем макс. элемент и его Id
+        group.setId(after.stream().max(Comparator.comparingInt(ProductData::getId)).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
     }
